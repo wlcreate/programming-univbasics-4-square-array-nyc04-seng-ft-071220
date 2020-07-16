636525de12e88c2c7cb540ec053fd360ce85487e
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 18:37:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while array[counter] do
+    new_array << array[counter] ** 2
+    counter += 1
+  end
+  new_array
 end
