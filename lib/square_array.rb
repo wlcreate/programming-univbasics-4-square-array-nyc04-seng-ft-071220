1c45b6f88c418bbc10b3e40f93d445f153c28f6f
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 18:40:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,4 @@
-def square_array(array)
-  new_array = []
-  counter = 0
-  while array[counter] do
-    new_array << array[counter] ** 2
-    counter += 1
-  end
-  new_array
-end
 
-#def square_array(array)
-  #new_array = []
-  #array.length.times do |element|
-    #new_array.push(array[element] ** 2)
-  #end
-  #new_array
-#end
 
 def square_array(array)
   new_array = []
