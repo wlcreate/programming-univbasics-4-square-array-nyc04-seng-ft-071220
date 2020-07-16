f610c081472bd4bbbc75c46917788bd3c59868be
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 18:46:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,27 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+  while array[counter] do
+    new_array << array[counter] ** 2
+    counter += 1
+  end
+  new_array
 end
+
+#def square_array(array)
+  #new_array = []
+  #array.length.times do |element|
+    #new_array.push(array[element] ** 2)
+  #end
+  #new_array
+#end
+
+#using the .each method
+#def square_array(array)
+  #new_array = []
+  #array.each do |num|
+  #square = num ** 2
+  #new_array << square
+#end
+#new_array
+#end
